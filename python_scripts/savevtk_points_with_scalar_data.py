d20387c844bc0ef83d703cd188565def5d05a379
Nick Battista <nickabattista@gmail.com>
2016-08-27 13:07:28 -0400

    updated savevtk_points_with_scalar_data

--- python_scripts/savevtk_points_with_scalar_data.py
+++ python_scripts/savevtk_points_with_scalar_data.py
@@ -30,7 +30,7 @@ def savevtk_points_with_scalar_data( X, scalarArray, filename, colorMap):
     # colorMap:  What you are naming the data you're printing (string)
 
     N = X.shape[0]
-    nx,ny = scalarArray.shape
+    nx = scalarArray.shape[0]
 
     if C_flag == True:
         nX = np.ascontiguousarray(X, dtype=np.float64)
@@ -46,11 +46,11 @@ def savevtk_points_with_scalar_data( X, scalarArray, filename, colorMap):
                 file.write('{0:.15e} {1:.15e} {2:.15e}\n'.format(X[ii,0],X[ii,1],X[ii,2]))
             file.write('\n')
             #
-            file.write('POINT_DATA   {0}\n'.format(nx*ny*1))
+            file.write('POINT_DATA   {0}\n'.format(nx*1*1))
             file.write('SCALARS '+colorMap+' double\n')
             file.write('LOOKUP_TABLE default\n')
             file.write('\n')
             for c in range(nx):
-                file.write('{0} '.format(scalarArray[c,0]))
+                file.write('{0} '.format(scalarArray[c]))
                 file.write('\n')
             #Python 3.5 automatically opens in text mode unless otherwise specified
